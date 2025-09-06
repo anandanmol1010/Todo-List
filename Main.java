dea0db3cb9f3016712cf3910653682723cbb2f17
Anmol Anand <anandanmol1010@gmail.com>
2025-09-06 21:44:15 +0530

    Changes

--- Main.java
+++ Main.java
@@ -55,6 +55,7 @@ class TodoList implements todoWorking{
             tasks.remove(r);
         }
         else {
+            Main.gap();
             System.out.println("Wrong Input!");
         }
     }
@@ -64,6 +65,7 @@ class TodoList implements todoWorking{
             tasks.get(r).markCompleted();
         }
         else {
+            Main.gap();
             System.out.println("Wrong Input!");
         }
     }
@@ -103,6 +105,7 @@ class TodoManage{
             all_lists.remove(r);
         }
         else {
+            Main.gap();
             System.out.println("Wrong Input!");
         }
     }
@@ -129,12 +132,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         TodoManage todoLists = new TodoManage();
         while (running) {
+            gap();
+
+            System.out.println("---------Todo-List------------------");
             System.out.println("1. Create Todo List");
             System.out.println("2. Use Todo List");
             System.out.println("3. Remove Todo List");
             System.out.println("4. Quit");
             System.out.print("Enter choice: ");
             int n = sc.nextInt();
+
+            gap();
 
             int i;
             boolean usingList;
@@ -148,6 +156,7 @@ public class Main {
 
                     usingList = true;
                     while (usingList) {
+                        gap();
 
                         TodoList selected = todoLists.getTodolist(i);
 
@@ -158,6 +167,8 @@ public class Main {
                         System.out.println("4. Quit");
                         System.out.print("Enter choice: ");
                         int c = sc.nextInt();
+
+                        gap();
 
                         switch (c) {
                             case 1:
@@ -202,9 +213,11 @@ public class Main {
 
                 case 2:
                     if (todoLists.isEmpty()) {
+                        gap();
                         System.out.println("List is Empty");
                     } else {
                         todoLists.showAllLists();
+
                         System.out.print("Choose list number: ");
                         i = sc.nextInt() - 1;
 
@@ -213,50 +226,58 @@ public class Main {
 
                             TodoList selected = todoLists.getTodolist(i);
 
-                            System.out.println("\n--- Using Todo List: " + selected.getName() + " ---");
-                            System.out.println("1. Add task");
-                            System.out.println("2. Remove task");
-                            System.out.println("3. Mark task as completed");
-                            System.out.println("4. Quit");
-                            System.out.print("Enter choice: ");
-                            int c = sc.nextInt();
+                            gap();
+                            if (selected.getName().isEmpty()) {
+                                System.out.println("Wrong Choice! Plz Retry\n");
+                            }
+                            else {
+                                System.out.println("--- Using Todo List: " + selected.getName() + " ---");
+                                System.out.println("1. Add task");
+                                System.out.println("2. Remove task");
+                                System.out.println("3. Mark task as completed");
+                                System.out.println("4. Quit");
+                                System.out.print("Enter choice: ");
+                                int c = sc.nextInt();
 
-                            switch (c) {
-                                case 1:
-                                    System.out.print("Enter task name: ");
-                                    String taskName = sc.next();
-                                    selected.addTask(taskName);
-                                    break;
+                                switch (c) {
+                                    case 1:
+                                        System.out.print("Enter task name: ");
+                                        String taskName = sc.next();
+                                        selected.addTask(taskName);
+                                        break;
 
-                                case 2:
-                                    if(selected.tasks.isEmpty()){
-                                        System.out.println("No Task found");
-                                    }
-                                    else {
-                                        selected.showTasks();
-                                        System.out.print("Enter task number to remove: ");
-                                        int r = sc.nextInt() - 1;
-                                        selected.removeTask(r);
-                                    }
-                                    break;
+                                    case 2:
+                                        if (selected.tasks.isEmpty()) {
+                                            gap();
+                                            System.out.println("No Task found");
+                                        } else {
+                                            selected.showTasks();
+                                            gap();
+                                            System.out.print("Enter task number to remove: ");
+                                            int r = sc.nextInt() - 1;
+                                            selected.removeTask(r);
+                                        }
+                                        break;
 
-                                case 3:
-                                    if(selected.tasks.isEmpty()){
-                                        System.out.println("No Task found");
-                                    }
-                                    else {
-                                        selected.showTasks();
-                                        System.out.print("Enter task number to mark completed: ");
-                                        int m = sc.nextInt() - 1;
-                                        selected.markCompleted(m);
-                                    }
-                                    break;
+                                    case 3:
+                                        if (selected.tasks.isEmpty()) {
+                                            gap();
+                                            System.out.println("No Task found");
+                                        } else {
+                                            selected.showTasks();
+                                            System.out.print("Enter task number to mark completed: ");
+                                            int m = sc.nextInt() - 1;
+                                            selected.markCompleted(m);
+                                        }
+                                        break;
 
-                                case 4:
-                                    usingList = false;
-                                    break;
-                                default:
-                                    System.out.println("Wrong Choice! Plz Retry\n");
+                                    case 4:
+                                        usingList = false;
+                                        break;
+                                    default:
+                                        gap();
+                                        System.out.println("Wrong Choice! Plz Retry\n");
+                                }
                             }
                         }
                     }
@@ -264,6 +285,7 @@ public class Main {
 
                 case 3:
                     if(todoLists.isEmpty()) {
+                        gap();
                         System.out.println("List is Empty");
                     }
 
@@ -280,8 +302,22 @@ public class Main {
                     break;
 
                 default:
+                    gap();
                     System.out.println("Wrong Choice! Plz Retry\n");
             }
         }
+    }
+
+    public static void gap(){
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
